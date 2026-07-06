ffd1099c18281991ed4ab96540067adb7ffa993d
ahmadShaalan <ahmad.kamal.shalan@outlook.com>
2026-07-06 20:29:26 +0300

    Update button text to include exclamation mark

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div>
       <button className="mt-4 ml-5 cursor-pointer rounded-lg bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700">
-        Click Me
+        Click Me!
       </button>
     </div>
   );
