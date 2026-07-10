5d6dda20924590c98cac8fa77fb451e998aa6f5b
ahmadShaalan <ahmad.kamal.shalan@outlook.com>
2026-07-10 16:04:46 +0300

    Remove password validation rules from login schema

--- src/features/auth/types/index.ts
+++ src/features/auth/types/index.ts
@@ -3,13 +3,12 @@ import { z } from 'zod';
 export const loginSchema = z.object({
   email: z.string().min(1, 'Email is required').email('Invalid email address'),
 
-  password: z
-    .string()
-    .min(8, 'Password must be at least 8 characters')
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).*$/,
-      'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
-    ),
+  password: z.string(),
+  // .min(8, 'Password must be at least 8 characters')
+  // .regex(
+  //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).*$/,
+  //   'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+  // ),
 });
 
 export type LoginFormValues = z.infer<typeof loginSchema>;
